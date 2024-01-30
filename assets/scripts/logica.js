6893acbd8238c4d6528491399e583b0022f2289e
cescriba-2022278 <cescriba-2022278@kinal.edu.gt>
2024-01-30 16:49:43 -0600

    archivos traspasados a su rama respectiva

--- assets/scripts/logica.js
+++ assets/scripts/logica.js
@@ -13,6 +13,17 @@ function redirectToDetalle3() {
     window.location.href  = "./detallesContacto/contactoDetalle3.html";
 }
 
+function redirectToDetalle4() {
+    alert("Redirigiendo a la página de detalle");
+    window.location.href  = "./detallesContacto/contactoDetalle4.html";
+}
+
+function redirectToDetalle5() {
+    alert("Redirigiendo a la página de detalle");
+    window.location.href  = "./detallesContacto/contactoDetalle5.html";
+}
+
+
 function redirectToAgregarContacto() {
     alert("Redirigiendo a la página de agregar contacto");
     window.location.href   = "agregar_contacto.html";
