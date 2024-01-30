65767c4a6e49aaa27e20a593e330d71f94f951e9
cescriba-2022278 <cescriba-2022278@kinal.edu.gt>
2024-01-30 15:54:41 -0600

    archivos de contacto en procesos de mejora

---
+++ assets/scripts/logica.js
@@ -0,0 +1,9 @@
+function redirectToDetalle1() {
+    alert("Redirigiendo a la página de detalle");
+    window.location.href  = "detalle1.html";
+}
+
+function redirectToAgregarContacto() {
+    alert("Redirigiendo a la página de agregar contacto");
+    window.location.href   = "agregar_contacto.html";
+}
